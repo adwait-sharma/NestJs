49ba24df31fdf22cc28a361e257b5e2a3d29fb17
unknown <sharma.adwait04@gmail.com>
2020-09-16 12:21:39 +0530

    Pipes, Validation & Error Handling

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { stringify } from 'querystring';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -10,7 +11,7 @@ export class TasksController {
     constructor (private tasksService:TasksService) {}
 
         @Get()
-        getTasks(@Query() filterDto: GetTasksFilterDto): Task[]{
+        getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[]{
             if(Object.keys(filterDto).length){
                 return this.tasksService.getTasksWithFilters(filterDto);
             }else{
@@ -20,6 +21,7 @@ export class TasksController {
         }
         
         @Post()
+        @UsePipes(ValidationPipe)
         createTask(@Body() createTaskDto: CreateTaskDto): Task{
             
                 return this.tasksService.createTask(createTaskDto);
@@ -36,7 +38,8 @@ export class TasksController {
         }
 
         @Patch('/:id/status')
-        updateTaskStatus(@Param('id') id:string, status: TaskStatus ): Task{
+        updateTaskStatus(@Param('id') id:string, 
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus ): Task{
             return this.tasksService.updateTaskStatus(id,status);
         }
 
